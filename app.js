25676ab9e534b391aa7d6eafb104f7812d15e856
Tunahan Tuna <mustafatunahantuna@outlook.com>
2023-07-25 18:26:26 +0300

    update: minor updates

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ import getData from "./get-data.js";
 (async () => {
   const data = await getData(1);
 
-  console.log(data);
+  data && console.log(data);
 })();
--- get-data.js
+++ get-data.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 const getData = async (Number) => {
-  const { data: user } = await axios(
+  const user = await axios(
     `https://jsonplaceholder.typicode.com/users/${Number.toString()}`
-  );
-  const { data: post } = await axios(
+  ).catch((err) => console.log("User is not found"));
+  const post = await axios(
     `https://jsonplaceholder.typicode.com/posts?userId=${Number.toString()}`
+  ).catch((err) => console.log("Post is not found"));
+  return (
+    user &&
+    user.data && { ...user.data, posts: post && post.data && [...post.data] }
   );
-  return { ...user, posts: [...post] };
 };
 export default getData;
